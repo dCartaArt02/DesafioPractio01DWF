a310efade812c15ece3c5423dd68af6dac73ceec
Zandelito <everzw1@gmail.com>
2025-08-01 20:40:53 -0300

    Revert "DESAFIO1_DWF"
    This reverts commit d38b15e8d415f901e6b392606be446dd04d83ae6.

--- bookapi/src/main/java/com/letrasvivas/bookapi/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.letrasvivas.bookapi.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
